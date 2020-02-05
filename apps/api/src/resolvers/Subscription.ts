22f1f588183a3b00d8619dc66d52ed5d7fbef71f
Vinicius Ledesma <viniciusledesma@yahoo.com.br>
2020-02-04 23:01:03 -0300

    Implementada a autenticação nas subscriptions.

--- apps/api/src/resolvers/Subscription.ts
+++ apps/api/src/resolvers/Subscription.ts
@@ -1,12 +1,34 @@
-import { Order, SubscriptionResolver } from '../types'
+import { withFilter } from 'graphql-yoga'
+import { Types } from 'mongoose'
+import {
+  Order,
+  Resolver,
+  SubscriptionArgs,
+  SubscriptionPayload,
+  SubscriptionResolver,
+  UserRole,
+} from '../types'
+
+const orderSubscribeFn: Resolver<SubscriptionArgs> = (_, args, ctx) => {
+  const { mutationIn } = args.where
+  const { pubsub } = ctx
+  const channels = mutationIn.map(m => `ORDER_${m}`)
+  return pubsub.asyncIterator(channels)
+}
+
+const orderFilterFn: Resolver<SubscriptionArgs, SubscriptionPayload<Order>> = (
+  payload,
+  args,
+  ctx,
+) => {
+  const { _id, role } = ctx.authUser
+  return role === UserRole.ADMIN
+    ? true
+    : (payload.node.user as Types.ObjectId).equals(_id)
+}
 
 const order: SubscriptionResolver<Order> = {
-  subscribe: (_, args, ctx) => {
-    const { mutationIn } = args.where
-    const { pubsub } = ctx
-    const channels = mutationIn.map(m => `ORDER_${m}`)
-    return pubsub.asyncIterator(channels)
-  },
+  subscribe: withFilter(orderSubscribeFn, orderFilterFn),
   resolve: payLoad => payLoad,
 }
 
