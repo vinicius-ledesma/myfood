ddba3e273ff51947fa500244424d9642f308adf2
Vinicius Ledesma <viniciusledesma@yahoo.com.br>
2020-02-09 19:57:52 -0300

    -Exemplo de .env -Correção no Docker compose -Playground liberado na produção

--- apps/api/src/config/serverConfig.ts
+++ apps/api/src/config/serverConfig.ts
@@ -4,7 +4,8 @@ const { NODE_ENV, CLIENT_HOSTS } = process.env
 
 const isProduction = NODE_ENV === 'production'
 
-const playground = isProduction ? false : '/'
+// const playground = isProduction ? false : '/'
+const playground = '/'
 
 const hosts = CLIENT_HOSTS || ['localhost:8100', '127.0.0.1:8100'].join('|')
 const protocols = isProduction ? 'https|wss' : 'http|ws'
